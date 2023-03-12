0c7850b44d77b23c654cec70f7ef1fbcd93f463a
hailang <a1936978077@hotmail.com>
2023-03-12 16:20:12 +0800

    refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,53 @@
-// export defer
-pub use scopeguard::*;
-pub use work_steal_queue::*;
+#![deny(
+    // The following are allowed by default lints according to
+    // https://doc.rust-lang.org/rustc/lints/listing/allowed-by-default.html
+    anonymous_parameters,
+    bare_trait_objects,
+    // box_pointers, // use box pointer to allocate on heap
+    // elided_lifetimes_in_paths, // allow anonymous lifetime
+    missing_copy_implementations,
+    missing_debug_implementations,
+    // missing_docs, // TODO: add documents
+    single_use_lifetimes, // TODO: fix lifetime names only used once
+    // trivial_casts,
+    trivial_numeric_casts,
+    // unreachable_pub, allow clippy::redundant_pub_crate lint instead
+    // unsafe_code,
+    unstable_features,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
+    variant_size_differences,
+
+    warnings, // treat all wanings as errors
+
+    clippy::all,
+    clippy::restriction,
+    clippy::pedantic,
+    // clippy::nursery, // It's still under development
+    clippy::cargo,
+    unreachable_pub,
+)]
+#![allow(
+    // Some explicitly allowed Clippy lints, must have clear reason to allow
+    clippy::blanket_clippy_restriction_lints, // allow clippy::restriction
+    clippy::implicit_return, // actually omitting the return keyword is idiomatic Rust code
+    clippy::module_name_repetitions, // repeation of module name in a struct name is not big deal
+    clippy::multiple_crate_versions, // multi-version dependency crates is not able to fix
+    clippy::missing_errors_doc, // TODO: add error docs
+    clippy::missing_panics_doc, // TODO: add panic docs
+    clippy::panic_in_result_fn,
+    clippy::shadow_same, // Not too much bad
+    clippy::shadow_reuse, // Not too much bad
+    clippy::exhaustive_enums,
+    clippy::exhaustive_structs,
+    clippy::indexing_slicing,
+    clippy::separated_literal_suffix, // conflicts with clippy::unseparated_literal_suffix
+    clippy::single_char_lifetime_names, // TODO: change lifetime names
+)]
 
 #[allow(dead_code)]
 pub mod coroutine;
-pub use coroutine::*;
 
-#[allow(dead_code)]
 pub mod scheduler;
-pub use scheduler::*;
-
-#[cfg(unix)]
-#[macro_export]
-macro_rules! shield {
-    () => {{
-        unsafe {
-            let mut set: libc::sigset_t = std::mem::zeroed();
-            libc::sigaddset(&mut set, libc::SIGURG);
-            let mut oldset: libc::sigset_t = std::mem::zeroed();
-            libc::pthread_sigmask(libc::SIG_SETMASK, &set, &mut oldset);
-            oldset
-        }
-    }};
-}
-
-#[cfg(unix)]
-#[macro_export]
-macro_rules! unbreakable {
-    ( $fn: expr ) => {{
-        let oldset = $crate::shield!();
-        unsafe {
-            let res = $fn;
-            libc::pthread_sigmask(libc::SIG_SETMASK, &oldset, std::ptr::null_mut());
-            res
-        }
-    }};
-}
-
-#[allow(dead_code)]
-mod monitor;
-
-#[allow(dead_code)]
-pub mod event_loop;
-pub use event_loop::event::*;
-pub use event_loop::interest::*;
-pub use event_loop::*;
-
-#[cfg(any(
-    target_os = "linux",
-    target_os = "l4re",
-    target_os = "android",
-    target_os = "emscripten"
-))]
-#[allow(dead_code)]
-pub mod epoll;
-#[cfg(any(
-    target_os = "linux",
-    target_os = "l4re",
-    target_os = "android",
-    target_os = "emscripten"
-))]
-pub use epoll::*;
